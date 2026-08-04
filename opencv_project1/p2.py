68bb737c41bd88d61689a0ba6a9cb805e86072b3
idkmynamemynameis <micemail86@gmail.com>
2026-08-04 12:00:12 -0600

    finds red

--- opencv_project1/p2.py
+++ opencv_project1/p2.py
@@ -2,14 +2,17 @@ import cv2 as cv
 import numpy as np
 def nothing(x):
     pass
-cv.imshow('control',nothing)
+
 cap=cv.VideoCapture(0)
-cv.setTrackbarPos('hue min','control',)
+
+
 while 1:
+    
+
     _,frame = cap.read()
     hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-    lower_red=np.array([100,100,100])
-    upper_red=np.array([250,255,255])
+    lower_red=np.array([166,150,200])
+    upper_red=np.array([255,255,255])
     mask=cv.inRange(hsv,lower_red,upper_red)
     res=cv.bitwise_and(frame,frame,mask=mask)
     cv.imshow('frame',frame)
