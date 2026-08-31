7633660190a20c9edf66daca4e2de37e7ffa5496
idkmynamemynameis <micemail86@gmail.com>
2026-08-31 08:25:35 -0600

    tk stuff

--- tkinter stuff/test_tkinter.py
+++ tkinter stuff/test_tkinter.py
@@ -1,16 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import showinfo
 #blurred text
 from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1)
 #init
 root = tk.Tk()
+def funct():
+    showinfo(
+        title='Test title',
+        message=str(counter),
+        
+    )
+#set counter
+counter=0
+def add_one():
+    global counter
+    counter+=1
+    counterstring.set(str(counter))
+    return
+#make button
+ttk.Button(
+   root, 
+   text="Click Me", 
+   command=funct
+).pack()
+#make strvar
+counterstring=tk.StringVar()
+counterstring.set(str(counter))
 
-
-
-tk.Label(root, text='Classic Label').pack()
-ttk.Label(root, text='Themed Label').pack()
-button=ttk.Button(root,text='Button').pack()
-ttk.Checkbutton(root,text='checkbutton').pack()
+ttk.Button(root,text='Add one',command=add_one).pack()
+clabel=ttk.Label(root, textvariable=counterstring).pack()
 
 root.mainloop()
