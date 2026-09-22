2fda138d2356d6b55f41a2010e069d87de248d0e
idkmynamemynameis <micemail86@gmail.com>
2026-09-22 12:35:32 -0600

    tidied up and i did use claude to find bugs but i fixed them because fuc ai

--- tkinter stuff/minesweeper.py
+++ tkinter stuff/minesweeper.py
@@ -1,9 +1,6 @@
-# Source - https://stackoverflow.com/a/7591453
-# Posted by Vaughn Cato, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-09-18, License - CC BY-SA 4.0
 
 from tkinter import * # type: ignore
-import os
+
 import random as rand
 import tkinter.messagebox 
 #init config
@@ -81,75 +78,6 @@ def check_around(x,y,clicked=False):
             
 
 
-
-'''      
-def echeck_around(x,y):
-    if not (0 <= x < x_len and 0 <= y < y_len):
-            return
-    #GET NAME
-    mbutton=buttons[x][y]
-
-    name=buttons[x][y].winfo_name()
-    #check if it's a bomb
-    if name[0] == 'n':
-        #if it is, die
-        print('d')
-        response = tkinter.messagebox.askyesno(
-                    "Game Over", " You Lost\n Do you want to replay?", icon='warning')
-        if response==YES:
-            make_board()
-        else:
-            root.destroy()
-    else:
-        #if not, check around
-        #make grid of spots to be checked
-        x_pos=[-1,0,1]
-        y_pos=[1,0,-1]
-        for i in range(len(x_pos)):
-            x_pos[i]+=x
-            y_pos[i]+=y
-        #count bombs
-        tot_bomb = 0
-        tot_bomb = 0
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                nx, ny = x + dx, y + dy
-                # Ensure neighboring cell exists inside grid
-                if 0 <= nx < x_len and 0 <= ny < y_len:
-                    if find_type(nx, ny) == 'yes':
-                        tot_bomb += 1
-        mbutton.checked=1
-        if tot_bomb == 0:
-            mbutton.config(image=empty_icon, text="", bg="lightgrey")
-            # Flood-fill: Reveal neighboring safe tiles recursively
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
-                    if dx == 0 and dy == 0:
-                        continue
-                    check_around(x + dx, y + dy)
-        elif tot_bomb == 1:
-            buttons[x][y].config(image=one_icon)
-        elif tot_bomb == 2:
-            buttons[x][y].config(image=two_icon)
-        else:
-            buttons[x][y].config(image=three_icon)
-        
-                        
-        
-def find_type(x,y):
-    if 0 < x < x_len and 0 < y < y_len:
-        try:
-            name=buttons[x][y].winfo_name()
-            
-            if name[0] == 'n':
-                return 'yes'
-            else:
-                
-                return 'no'
-        except:
-            return 'oob'
-    '''
-#make the empty array
 def clear_buttons():
     global buttons
     buttons=[[] for _ in range(x_len)]
