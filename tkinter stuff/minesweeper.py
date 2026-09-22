9a76a1a095a2d664ffff50e657dff6a7ee33e601
idkmynamemynameis <micemail86@gmail.com>
2026-09-22 12:34:30 -0600

    it works!!!

--- tkinter stuff/minesweeper.py
+++ tkinter stuff/minesweeper.py
@@ -2,7 +2,7 @@
 # Posted by Vaughn Cato, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-09-18, License - CC BY-SA 4.0
 
-from tkinter import *
+from tkinter import * # type: ignore
 import os
 import random as rand
 import tkinter.messagebox 
@@ -26,12 +26,69 @@ bomb_icon=PhotoImage(file='./tkinter stuff/bomb.png')
 one_icon=PhotoImage(file='./tkinter stuff/1_icon.png')
 two_icon=PhotoImage(file='./tkinter stuff/2.png')
 three_icon=PhotoImage(file='./tkinter stuff/3.png')
+four_icon=PhotoImage(file='./tkinter stuff/3.png')
 #magically look around
-def check_around(x,y):
+def check_around(x,y,clicked=False):
+    if not (0 <= x < x_len and 0 <=y < y_len):
+         print('oob')
+         return
+    if buttons[x][y].checked==1:
+        print('checked')
+        return
+    button=buttons[x][y]
+    if  str(button.winfo_name()).startswith('n'):
+        if clicked:
+            buttons[x][y].config(image=bomb_icon)
+            response = tkinter.messagebox.askyesno("Game Over", " You Lost\n Do you want to replay?", icon='warning')
+            if response==YES:
+                make_board()
+                return
+            else:
+                root.destroy()
+                return
+        else:
+            return
+    
+        
+
+    tot_bombs=0
+    #rest of code
+    for ax in [-1,0,1]:
+        ax+=x
+        for ay in [1,0,-1]:
+            ay+=y
+            if 0 <= ax < x_len and 0 <= ay < y_len:
+                question_button=buttons[ax][ay]
+                if str(question_button.winfo_name()).startswith('n'):
+                    tot_bombs+=1
+    buttons[x][y].checked=1
+
+    if tot_bombs==0:
+        buttons[x][y].config(image=empty_icon)
+        for bx in [-1,0,1]:
+            bx+=x
+            for by in [1,0,-1]:
+                by+=y
+                check_around(bx,by)
+    elif tot_bombs == 1:
+        buttons[x][y].config(image=one_icon)
+    elif tot_bombs == 2:
+        buttons[x][y].config(image=two_icon)
+    elif tot_bombs == 3:
+        buttons[x][y].config(image=three_icon)
+    elif tot_bombs == 4:
+        buttons[x][y].config(image=four_icon)
+            
+
+
+
+'''      
+def echeck_around(x,y):
     if not (0 <= x < x_len and 0 <= y < y_len):
             return
     #GET NAME
     mbutton=buttons[x][y]
+
     name=buttons[x][y].winfo_name()
     #check if it's a bomb
     if name[0] == 'n':
@@ -52,6 +109,7 @@ def check_around(x,y):
             x_pos[i]+=x
             y_pos[i]+=y
         #count bombs
+        tot_bomb = 0
         tot_bomb = 0
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
@@ -90,7 +148,7 @@ def find_type(x,y):
                 return 'no'
         except:
             return 'oob'
-    
+    '''
 #make the empty array
 def clear_buttons():
     global buttons
@@ -107,14 +165,14 @@ def make_board():
             id+=1
             #pick a name
             if not rand.randint(0,10):
-                icon=bomb_icon
+                icon=full_icon
                 name='not safe'+str(id)
             else:
                 icon=full_icon
                 name='safe'+str(id)
             #make button
-            btn = Button(frame,image=icon,name=name,command=lambda x=x,y=y:check_around(x,y))
-            btn.checked=0
+            btn = Button(frame,image=icon,name=name,command=lambda x=x,y=y,t=True:check_around(x,y,t))
+            btn.checked=0 # type: ignore
             #add button to array
             buttons[x].append(btn)
             #add button to screen
