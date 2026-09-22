35173e82fff28a4210ec65e51fa5d4ab1ef5a34c
idkmynamemynameis <micemail86@gmail.com>
2026-09-22 15:54:02 -0600

    made new pictures and let you win the game

--- tkinter stuff/minesweeper.py
+++ tkinter stuff/minesweeper.py
@@ -4,7 +4,7 @@ from tkinter import * # type: ignore
 import random as rand
 import tkinter.messagebox 
 #init config
-x_len=5
+x_len=7
 y_len=5
 #init tk stuff
 root = Tk()
@@ -17,15 +17,17 @@ grid.grid(sticky="news", column=0, row=x_len, columnspan=2)
 frame.rowconfigure(x_len, weight=1)
 frame.columnconfigure(0, weight=1)
 #grabs icons
-empty_icon=PhotoImage(file='./tkinter stuff/empty.png')
-full_icon = PhotoImage(file='./tkinter stuff/full.png')
+empty_icon=PhotoImage(file='./tkinter stuff/clicked-.png')
+full_icon = PhotoImage(file='./tkinter stuff/unclicked.png')
 bomb_icon=PhotoImage(file='./tkinter stuff/bomb.png')
-one_icon=PhotoImage(file='./tkinter stuff/1_icon.png')
+one_icon=PhotoImage(file='./tkinter stuff/1.png')
 two_icon=PhotoImage(file='./tkinter stuff/2.png')
 three_icon=PhotoImage(file='./tkinter stuff/3.png')
 four_icon=PhotoImage(file='./tkinter stuff/3.png')
 #magically look around
+nchecked=0
 def check_around(x,y,clicked=False):
+    global nbombs,nchecked
     if not (0 <= x < x_len and 0 <=y < y_len):
          print('oob')
          return
@@ -45,8 +47,9 @@ def check_around(x,y,clicked=False):
                 return
         else:
             return
+    else:
+        nchecked+=1
     
-        
 
     tot_bombs=0
     #rest of code
@@ -62,6 +65,8 @@ def check_around(x,y,clicked=False):
 
     if tot_bombs==0:
         buttons[x][y].config(image=empty_icon)
+        
+        
         for bx in [-1,0,1]:
             bx+=x
             for by in [1,0,-1]:
@@ -75,17 +80,30 @@ def check_around(x,y,clicked=False):
         buttons[x][y].config(image=three_icon)
     elif tot_bombs == 4:
         buttons[x][y].config(image=four_icon)
+    if nchecked == (x_len*y_len)-nbombs:
+        buttons[x][y].config(image=empty_icon)
+        response = tkinter.messagebox.askyesno("You Won", " You won!\n Do you want to replay?", icon='question')
+        if response==YES:
+            make_board()
+            return
+        else:
+            root.destroy()
+            return
+
+        
             
 
 
 def clear_buttons():
     global buttons
     buttons=[[] for _ in range(x_len)]
-
+nbombs=0
 buttons=[[] for _ in range(x_len)]
 def make_board():
+    global nbombs,nchecked
     clear_buttons()
     #make the buttons
+    nbombs,nchecked=0,0
     id=0
     for x in range(x_len):
         for y in range(y_len):
@@ -95,6 +113,7 @@ def make_board():
             if not rand.randint(0,10):
                 icon=full_icon
                 name='not safe'+str(id)
+                nbombs+=1
             else:
                 icon=full_icon
                 name='safe'+str(id)
